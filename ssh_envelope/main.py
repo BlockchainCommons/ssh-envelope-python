72332f2cdf7fbea287bbcbf9bba738d2a9b58b5e
Wolf McNally <wolf@wolfmcnally.com>
2024-03-21 15:28:52 -0700

    Working.

--- ssh_envelope/main.py
+++ ssh_envelope/main.py
@@ -2,22 +2,29 @@ import logging
 import argparse
 import sys
 
-from . import logconfig
+from ssh_envelope import logconfig
 __all__ = ['logconfig']
 
-from .ssh_object_utils import import_ssh_object
+from ssh_envelope.ssh_object_utils import import_ssh_object
 
 logger = logging.getLogger(__name__)
 
 def import_object(args):
     logger.info(f"Importing object")
-    data = args.data
+    object_data = None
 
-    if data is None:
+    if args.object:
+        logger.info("Reading data from --object")
+        object_data = args.object
+    elif args.file:
+        logger.info("Reading data from --file")
+        with open(args.file, 'r') as file:
+            object_data = file.read()
+    else:
         logger.info("Reading data from stdin")
-        data = sys.stdin.read()
+        object_data = sys.stdin.read()
 
-    envelope = import_ssh_object(data)
+    envelope = import_ssh_object(object_data)
     if envelope is None:
         raise ValueError("Failed to import SSH object")
 
@@ -32,14 +39,15 @@ def main():
     subparsers = parser.add_subparsers(help='commands')
 
     # import_object command
-    parser_import_public = subparsers.add_parser('import', help='Convert an SSH object to an envelope')
-    parser_import_public.add_argument('data', nargs='?', default=None, help='Object to import')
-    parser_import_public.set_defaults(func=import_object)
+    parser_import = subparsers.add_parser('import', help='Convert an SSH object to an envelope')
+    parser_import.add_argument('--object', help='SSH object as a string', default=None)
+    parser_import.add_argument('--file', help='Path to the file containing the SSH object', default=None)
+    parser_import.set_defaults(func=import_object)
 
     # export_object command
-    parser_export_public = subparsers.add_parser('export', help='Convert an envelope to an SSH object')
-    parser_export_public.add_argument('envelope', help='Envelope to export')
-    parser_export_public.set_defaults(func=export_object)
+    parser_export = subparsers.add_parser('export', help='Convert an envelope to an SSH object')
+    parser_export.add_argument('envelope', help='Envelope to export')
+    parser_export.set_defaults(func=export_object)
 
     args = parser.parse_args()
     if hasattr(args, 'func'):
